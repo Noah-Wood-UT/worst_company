b592d546dcd1947c25c62594a8f8d01e9fa05c0b
Noah Wood <noah.wood@utexas.edu>
2025-03-06 16:48:17 -0600

    Add files via upload

--- employees.py
+++ employees.py
@@ -2,7 +2,7 @@
 Student information for this assignment:
 
 Replace <FULL NAME> with your name.
-On my/our honor, <FULL NAME> and <FULL NAME>, this
+On my/our honor, Noah Wood and Luis Higareda, this
 programming assignment is my own work and I have not provided this code to
 any other student.
 
@@ -13,7 +13,7 @@ code to someone else), the case shall be submitted to the Office of the Dean of
 Students. Academic penalties up to and including an F in the course are likely.
 
 UT EID 1: nww376
-UT EID 2:
+UT EID 2: leh3479
 """
 
 from abc import ABC, abstractmethod
